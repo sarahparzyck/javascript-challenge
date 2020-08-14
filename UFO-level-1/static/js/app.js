efcb90743b5a9dccde681f4182945c42a7fa49ca
Sarah Parzyck <sarahparzyck@gmail.com>
2020-08-14 17:15:15 -0500

    partial complete

---
+++ UFO-level-1/static/js/app.js
@@ -0,0 +1,17 @@
+// from data.js
+var tableData = data;
+
+// YOUR CODE HERE!
+// Get a reference to the table body
+var tbody = d3.select("tbody");
+
+// Console.log the UFO data from data.js
+console.log(tableData);
+
+tableData.forEach((UFOReport) => {
+  var row = tbody.append("tr");
+  Object.entries(UFOReport).forEach(([key, value]) => {
+    var cell = row.append("td");
+    cell.text(value);
+  });
+});
