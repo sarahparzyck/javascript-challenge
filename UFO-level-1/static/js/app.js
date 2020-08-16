32b60d0f44a3974f8a7c235da9eb12e444157214
Sarah Parzyck <sarahparzyck@gmail.com>
2020-08-15 21:06:34 -0500

    final

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,7 +1,6 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
 // Get a reference to the table body
 var tbody = d3.select("tbody");
 
@@ -15,3 +14,38 @@ tableData.forEach((UFOReport) => {
     cell.text(value);
   });
 });
+
+// Select the button
+var button = d3.select("#filter-btn");
+
+// Select the form
+var form = d3.select("#form");
+
+// Create event handlers for clicking the button or pressing the enter key
+button.on("click", runEnter);
+form.on("submit",runEnter);
+
+// Create the function to run for both events
+function runEnter() {
+
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+
+  // Select the input element and get the raw HTML node
+  var input, filter, table, tr, td, i;
+  input = document.getElementById("datetime");
+  filter = input.value;
+  table = document.getElementById("ufo-table");
+  tr = table.getElementsByTagName("tr");
+
+  // Loop through all table rows
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if(td) {
+      if (td.innerHTML.indexOf(filter) > -1) {
+          tr[i].style.display = "";
+      } else {
+          tr[i].style.display = "none";
+      }      
+  }
+}};
